e21df9cf64e695a34b7d506e52a80dca33d4fea9
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2021-10-13 10:14:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Calcolatrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double first, second, soluction = 0; 
+            String operator;
+            Console.Write("Insert the first number ");
+            first = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Insert the second number ");
+            second = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Insert the operator to use ");
+            operator = Console.ReadLine();
+
+            switch (operator)
+            {
+                case "-":
+                    soluction = first - second;
+                    break;
+                case "*":
+                    soluction = first * second;
+                    break;
+                case "/":
+                    soluction = first / second;
+                    break;
+                case "+":
+                    soluction = first + second;
+                    break;
+                default:
+                    Console.WriteLine("L'operatore inserito non è corretto :/");
+                    break;
+            }
+            Console.WriteLine($"La soluzione è {soluction}");
+        }
+    }
+}
